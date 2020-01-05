3d6dfa3567968cd723ceb33b9276837d1c6e7a35
Martynas Jonaitis <marjon3@ktu.lt>
2020-01-05 22:33:59 +0200

    Papildytas shell kad istrintu folderi jeigu jis egzistuoja

--- data/5_predict_all.sh
+++ data/5_predict_all.sh
@@ -1,1 +1,2 @@
+rm -r ./output/prediction
 spark-submit ../src/pyspark-jobs/prediction.py ./output/joined/ ./parameters.json ./output/prediction/
--- data/6_predict_by_cluster.sh
+++ data/6_predict_by_cluster.sh
@@ -1,1 +1,2 @@
+rm -r ./output/prediction_by_clusters
 spark-submit ../src/pyspark-jobs/prediction_clusters.py ./output/k_means/clustered_kmeans__k_5_parquet/ ./parameters.json ./output/prediction_by_clusters/
